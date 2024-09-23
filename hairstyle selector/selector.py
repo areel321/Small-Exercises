12e65db99e3039dd19d2554af4a8222ea7d00b3b
areel321 <112980267+areel321@users.noreply.github.com>
2024-09-23 19:35:36 -0400

    all done!

--- hairstyle selector/selector.py
+++ hairstyle selector/selector.py
@@ -7,31 +7,46 @@ import pandas as pd
 from graphics import * 
 
 def build_graphic(win):
-    pass
+    heading = Text(Point(50, 90), "Our style for today is")
+    heading.setTextColor("purple")
+    heading.setStyle("bold")
+    heading.setSize(20)
+    heading.draw(win)
+    return
 
-def generate_style():
-    accessory = random.randint(0,9)
-    shape = random.randint(0,9)
-    extra = random.randint(0,9)
+def generate_style(styles):
+    accessory = styles.at[random.randint(0,9), "accessories"]
+    shape = styles.at[random.randint(0,9), "shapes"]
+    extra = styles.at[random.randint(0,9), "extras"]
     return(accessory, shape, extra)
 
     # print("Today let's do a " + styles.at[shape, "shapes"] + " style with "+styles.at[accessory, "accessories"]+" and "+styles.at[extra, "extras"])
 
 
-def populate_graphic(accessory, shape, extra):
-    pass
+def populate_graphic(accessory, shape, extra, win):
+    accessorytext = Text(Point(50, 80), "a "+shape+" style")
+    shapetext = Text(Point(50, 75), "with "+accessory+" accessories")
+    extratext = Text(Point(50, 70), "and "+extra+" embellishment")
+
+    accessorytext.draw(win)
+    shapetext.draw(win)
+    extratext.draw(win)
 
 def main():
-    # load in the file  
-    styles = pd.read_csv("../styles.csv")
+    # load in the file  hairstyle selector\styles.csv
+    styles = pd.read_csv("hairstyle selector/styles.csv")
     # build the graphic
     #Create a white window for weekly calendar
     win = GraphWin('hairstyle', 550, 550)
     win.setBackground('light pink')
-    win.setCoords(-100,-100,100,100)
+    win.setCoords(0,0,100,100)
     build_graphic(win)
-    accessory, shape, extra = generate_style()
-    populate_graphic(accessory, shape, extra)
+    accessory, shape, extra = generate_style(styles)
+    populate_graphic(accessory, shape, extra, win)
+
+    p = win.getMouse()
+    # Close the window
+    win.close()
 
 
 

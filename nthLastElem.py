3bccaba5a8baa1423b7437f1ebb26eef5a68a5fc
Allison Reel <areel2003@gmail.com>
2025-11-02 13:56:35 -0500

    Create nthLastElem.py

---
+++ nthLastElem.py
@@ -0,0 +1,27 @@
+# Given a singly linked list l and an integer n, return the value of the nth node from the end of the list. For n=1, return the last node of the list.
+
+# Note: in examples below and tests preview linked lists are presented as arrays just for simplicity of visualization: in real data you will be given a head node l of the linked list.
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(l, n):
+    
+    curr = l # get head of linked list
+    new = [] # empty list
+    new.append(curr.value) # add head value
+    helper(curr, new) # call helper
+    new = new[::-1] # reverse list
+    print(new)
+    return new[n-1] # return nth element from end
+    
+    
+def helper(curr, new):
+    if curr.next: # if there is another element, add it's value to the list
+        curr = curr.next
+        new.append(curr.value)
+        helper(curr, new)
+        

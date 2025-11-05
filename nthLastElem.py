8a119e1149762985b9f3d94aaee1ca72e9b21698
Allison Reel <areel2003@gmail.com>
2025-11-05 11:15:41 -0500

    Update nthLastElem.py

--- nthLastElem.py
+++ nthLastElem.py
@@ -24,4 +24,34 @@ def helper(curr, new):
         curr = curr.next
         new.append(curr.value)
         helper(curr, new)
+
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(l, n):
+    
+    curr = l # get head of linked list
+    new = [] # empty list - Max size of n # [5, 4, 3]
+    new.append(curr.value) # add head value
+    helper(curr, new, n) # call helper
+    return new[0]
+    print(new)
+    return new[n-1] # return nth element from end
+    
+    
+def helper(curr, new, n):
+    if curr.next: # if there is another element, add it's value to the list
+        curr = curr.next
+        new.append(curr.value)
+        if len(new) > n:
+            new.pop(0)
+        helper(curr, new, n)
+    
         
+        
+# For l = [5, 4, 3] and n = 1, the output should be solution(l, n) = 3;
+# For l = [5, 4, 3] and n = 3, the output should be solution(l, n) = 5.

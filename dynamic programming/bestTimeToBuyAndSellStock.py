c4131dbce5a33ed82aef1bde9a38ecc41d00e626
Allison Reel <areel2003@gmail.com>
2025-11-20 16:05:18 -0500

    Create bestTimeToBuyAndSellStock.py

---
+++ dynamic programming/bestTimeToBuyAndSellStock.py
@@ -0,0 +1,32 @@
+'''
+121. Best Time to Buy and Sell Stock
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=problem-list-v2&envId=dynamic-programming
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+'''
+
+
+
+'''
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        greatestPrice = 0
+       
+        for left in range(0,len(prices)-1):
+            for right in range(left+1,len(prices)):
+                greatestPrice = max(prices[right]-prices[left], greatestPrice)
+                if greatestPrice > 0:
+                    print(left, right)
+            
+            
+            
+        return greatestPrice
+
+'''

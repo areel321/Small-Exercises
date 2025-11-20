df53db88f4e7512e4e5138b534a266b9c1d2030d
Allison Reel <areel2003@gmail.com>
2025-11-20 16:06:14 -0500

    Create editDistance.py

---
+++ dynamic programming/editDistance.py
@@ -0,0 +1,11 @@
+'''
+72. Edit Distance
+https://leetcode.com/problems/edit-distance/description/?envType=problem-list-v2&envId=dynamic-programming
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+'''

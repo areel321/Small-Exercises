dcfa81ee6827acb88955ef1e0e3d52fc145cab83
Allison Reel <areel2003@gmail.com>
2025-11-20 16:04:12 -0500

    Update and rename editDistance.py to minCostClimbingStairs.py

--- dynamic programming/minCostClimbinfStairs.py
+++ dynamic programming/minCostClimbinfStairs.py
@@ -1,6 +1,6 @@
 '''
 746. Min Cost Climbing Stairs
-https://leetcode.com/problems/edit-distance/description/?envType=problem-list-v2&envId=dynamic-programming
+https://leetcode.com/problems/min-cost-climbing-stairs/description/?envType=problem-list-v2&envId=dynamic-programming
 You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
 
 You can either start from the step with index 0, or the step with index 1.

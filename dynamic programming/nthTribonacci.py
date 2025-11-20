77d2e73f6b57cb0c9fee85378bc57a4aa4181a8f
Allison Reel <areel2003@gmail.com>
2025-11-20 16:02:35 -0500

    Create nthTribonacci.py

---
+++ dynamic programming/nthTribonacci.py
@@ -0,0 +1,29 @@
+'''
+1137. N-th Tribonacci Number
+
+The Tribonacci sequence Tn is defined as follows: 
+
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+Given n, return the value of Tn.
+'''
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 1
+        array = [0] * (n+1)
+        array[0] = 0
+        array[1] = 1
+        array[2] = 1
+        for i in range(2, (n+1)):
+            array[i] = array[i-1] +  array[i-2] + array[i-3]
+        return array[n]
+        

89d8dc69bfb8f8e6b2cea859e8d6febeb3c2e450
Allison Reel <areel2003@gmail.com>
2025-11-30 13:15:34 -0500

    Update jumpGame.py

--- dynamic programming/jumpGame.py
+++ dynamic programming/jumpGame.py
@@ -6,6 +6,37 @@ You are given an integer array nums. You are initially positioned at the array's
 Return true if you can reach the last index, or false otherwise.
 '''
 class Solution(object):
+    '''
+    DP definition: The farthest index we can reach given allowed steps from 0 to i
+DP decision & relationship: It's either the dp[i - 1] or i + nums[i] whichever one is larger
+DP condition:
+If at any moment, dp[i] = 0, that means there is no way it can reach any further, return False immediately.
+If at any moment, dp[i] >= last index, that means it can already reach the end of the array given the steps allowed from 0 to i, return True immediately.
+'''
+
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        arr = [0] * len(nums)
+
+        arr[0] = nums[0]
+
+        for i in range(0,len(nums)-1):
+            if arr[i-1] < i:
+                return False 
+            arr[i] = max(i+nums[i], arr[i-1])
+            if arr[i] >= len(nums)-1:
+                return True
+        return arr[len(nums)-2]>= len(nums)-1
+
+
+
+
+'''
+class Solution(object):
 
     def canJump(self, nums):
         """
@@ -28,4 +59,4 @@ class Solution(object):
             if self.helper(nums, npos, nums[npos]):
                 return True
         return False
-        
+    '''   

125b8400b181d640e282d45fe93d415081789696
Allison Reel <areel2003@gmail.com>
2025-11-30 13:23:05 -0500

    Update editDistance.py

--- dynamic programming/editDistance.py
+++ dynamic programming/editDistance.py
@@ -9,3 +9,37 @@ Insert a character
 Delete a character
 Replace a character
 '''
+# https://leetcode.com/problems/edit-distance/solutions/159295/python-solutions-and-intuition-by-anders-amxq
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        dp = [[0] *(len(word2)+1) for _ in range(len(word1)+1)]
+
+        for i in range(0, len(word1)+1):
+            dp[i][0] = i
+        for j in range(0, len(word2)+1):
+            dp[0][j] = j
+        
+        for i in range(1, len(word1)+1):
+            for j in range(1, len(word2)+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+        
+        return dp[-1][-1]
+
+        
+    def insert(self, w, c, p):
+        return w[:p] + c + w[p:]
+
+    def delete(self, w, c):
+        return w.replace(c, "")
+
+    def replace(self, w, c, p):
+        return w.replace(p, c)
+    
